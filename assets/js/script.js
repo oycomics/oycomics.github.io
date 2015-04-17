413132f17fbce4263dd1ac3c110ecc1fbc6d57f8
Danny Burleson <danny@oycomics.com>
2015-04-17 16:23:10 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 $( document ).ready(function() {
 
-$('body').scrollspy({ target: '#bs-example-navbar-collapse-1' });
+$("body").scrollspy({ target: "#bs-example-navbar-collapse-1" });
 
+$("#bs-example-navbar-collapse-1 ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
 
 });
